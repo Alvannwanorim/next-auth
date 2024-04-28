adfeacaced6566f1e134735e06518ff597ecfe44
alvannwanorim <alvannwanorim@gmail.com>
2024-04-28 01:55:16 +0100

    Feat: prisma, db setup

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,33 @@
 "use server";
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
-export const register = async (values: z.infer<typeof LoginSchema>) => {
-  const validatedFields = LoginSchema.safeParse(values);
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
   //   return { error: "Invalid fields!" };
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
-  return { success: "Email sent!" };
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return { success: "User created" };
 };
